f311082361d385130676c04d1e8e6cd49509667c
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-07-05 21:24:57 -0500

    functions and objects

---
+++ chapter 1 /chapter 2 - javascript/objects.js
@@ -0,0 +1,24 @@
+//  object literals happens by listing its properties within braces
+
+const object1 = {
+  name: 'Bilal Shahzad',
+  age: 21,
+  education: 'WSU',
+}
+
+const object2 = {
+  name: 'Full Stack web application development',
+  level: 'intermediate studies',
+  size: 5,
+}
+
+const object3 = {
+  name: {
+    first: 'Dan',
+    last: 'Abramov',
+  },
+  grades: [2, 3, 5, 3],
+  department: 'Stanford University',
+}
+// The values of the properties can be of any type, like integers, strings, arrays, objects...
+
