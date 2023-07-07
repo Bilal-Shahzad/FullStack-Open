f1942a47ac018c4aebcb4eccf3226a96828742e0
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-07-06 22:23:26 -0500

    methods and exercises for js

---
+++ chapter 1 /javascript/exercies.js
@@ -0,0 +1,8 @@
+// a good way to make more things clear is console log
+
+const Header = (props) => {
+
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+  
---
+++ chapter 1 /javascript/methods.js
@@ -0,0 +1,24 @@
+// arrow functions are defined by using a function keywords that vary on how they react 
+// assing mehtods to anobject by defining properties that are functions 
+const Bilal = {
+    name: 'Bilal',
+    age:21,
+    education: 'WSU',
+  
+    greet: function() {
+      console.log('hello, my name is ' + this.name)
+    },
+  }
+  
+  Bilal.greet() 
+// "hello, my name is Bilal" gets printed
+
+// methods can be given to objects aftet the creation of it 
+
+Bilal.growOlder = function() {
+    this.age += 1
+  }
+
+  console.log(Bilal.age)   // 35 is printed
+  Bilal.growOlder()
+  console.log(Bilal.age)   // 36 is printed
