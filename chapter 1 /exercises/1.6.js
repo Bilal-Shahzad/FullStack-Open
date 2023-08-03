18de0d0845b00ce1e363de35dbbfecaebbc6e018
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-08-03 18:43:15 -0500

    react documentation and halfway through the exercises

---
+++ chapter 1 /exercises/1.6.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = (type) => {
+    // Update the corresponding state based on the feedback type
+    if (type === 'good') {
+      setGood(good + 1);
+    } else if (type === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (type === 'bad') {
+      setBad(bad + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Customer Feedback</h1>
+      {/* have added three buttons for each feedback category */}
+      {/*  when a button is clicked, the corresponding state is updated using the handleFeedback function */}
+      <button onClick={() => handleFeedback('good')}>Good</button>
+      <button onClick={() => handleFeedback('neutral')}>Neutral</button>
+      <button onClick={() => handleFeedback('bad')}>Bad</button>
+
+      <h2>Feedback Summary</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ chapter 1 /exercises/1.7.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = (type) => {
+    // Update the corresponding state based on the feedback type
+    if (type === 'good') {
+      setGood(good + 1);
+    } else if (type === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (type === 'bad') {
+      setBad(bad + 1);
+    }
+  };
+
+  const totalFeedback = good + neutral + bad;
+  const averageScore = (good - bad) / totalFeedback || 0;
+  const positivePercentage = (good / totalFeedback) * 100 || 0;
+
+  return (
+    <div>
+      <h1>Customer Feedback</h1>
+      <button onClick={() => handleFeedback('good')}>Good</button>
+      <button onClick={() => handleFeedback('neutral')}>Neutral</button>
+      <button onClick={() => handleFeedback('bad')}>Bad</button>
+
+      <h2>Feedback Summary</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total feedback: {totalFeedback}</p>
+      <p>Average score: {averageScore.toFixed(2)}</p>
+      <p>Positive feedback percentage: {positivePercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
+
+export default App;
+// calculated the total number of collected feedback, avg score, and percentage of positive feed back 
