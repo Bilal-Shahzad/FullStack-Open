3ea68ad14dc9cf4ea2825156c6d40b491b6f0a28
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-08-17 21:49:25 -0500

    exercies

--- chapter 1 /my-app/src/App.js
+++ chapter 1 /my-app/src/App.js
@@ -1,46 +1,137 @@
 import React from 'react';
-import Course from './course'; // Import the Course component
+// import Bilal from './blank'; 
+// function throwaway() {
+//   return (
+//     <div>
+//       <h1>Welcome to My App</h1>
+//       <Bilal > </Bilal>
+//     </div>
+//   );
+// }
+// export default throwaway;
+
+
+
+const Filter = ({ searchTerm, handleSearchChange }) => {
+  return (
+    <div>
+      filter shown with <input value={searchTerm} onChange={handleSearchChange} />
+    </div>
+  );
+};
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ personsToShow }) => {
+  return (
+    <ul>
+      {personsToShow.map((person) => (
+        <li key={person.id}>
+          {person.name} - {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        // ... other parts
-      ],
-    },
-    // ... other courses
-  ];
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123-456-7890', id: 1 },
+    { name: 'Ada Lovelace', number: '111-222-3333', id: 2 },
+    { name: 'Dan Abramov', number: '444-555-6666', id: 3 },
+    { name: 'Mary Poppendieck', number: '777-888-9999', id: 4 }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+    setNewNumber('');
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      {courses.map(course => (
-        <Course key={course.id} course={course} />
-      ))}
+      <h2>Phonebook</h2>
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
     </div>
   );
 };
 
 export default App;
 
-// Deines the course along with header, part,and content
 
 
-// 2.2 
-// changed content to calculate the total exercises by using the reduce function 
+// 2.6
+// using usestate to make the persons array and the newName innput
+// defined two event addPerson and handleNameChange for updating newName 
 
-// 2.3
-// reduce function  inside the content component uses longer form of the arrow function 
-// helps see values of total and the current part
 
-// 2.4
-// app maps through courses array and loads a course component for each course
+// 2.7
+// use the some method to check if there is a matching name in the persons array 
 
-// 2.5
-// create a course.js file and import it 
+// 2.8
+// newNumber to manage the phone number input 
+// handleNumberZChange that updates newNumber as the peron types 
+
+
+// 2.9
+// searchTerm to manage the search input field 
+// added handleSearchChange that updates searchTerm 
+
+// 2.10
+// Filter, PersonForm and Persons got extracted from App 
---
+++ chapter 1 /my-app/src/blank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bilal() {
+  return (
+    <div>
+      <h2>My name is Bilal</h2>
+    </div>
+  );
+}
+
+export default Bilal;
