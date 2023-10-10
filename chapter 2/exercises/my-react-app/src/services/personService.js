feedcc287f971190e3ccebe49fb498f2981bffb3
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-10-09 21:25:15 -0500

    fixing person.js so it creates  and error whenever you try to find a person and the person is deleted crea ting an error message  and immporting it into app.js

--- chapter 2/exercises/my-react-app/src/services/personService.js
+++ chapter 2/exercises/my-react-app/src/services/personService.js
@@ -1,26 +1,145 @@
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import personService from './services/personService';
+import Filter from './filter';
+import PersonForm from './personform';
+import Persons from './Persons';
 
-const baseUrl = 'http://localhost:3001/persons';
+const App = () => {
+  // State to manage the list of persons
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
 
-const create = (newPerson) => {
-  const request = axios.post(baseUrl, newPerson);
-  return request.then(response => response.data);
+  useEffect(() => {
+    personService.getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const addPerson = () => {
+    const existingPerson = persons.find(person => person.name === newName);
+
+    if (existingPerson) {
+      const confirmUpdate = window.confirm(
+        `${newName} is already added to the phonebook. Do you want to update the number?`
+      );
+
+      if (!confirmUpdate) {
+        return;
+      }
+
+      // If  person confirms update the number using HTTP PUT
+      updatePerson(existingPerson.id, newNumber);
+    } else {
+      // If the person doon't exist go with adding a new person
+      const newPerson = { name: newName, number: newNumber };
+
+      personService
+        .create(newPerson)
+        .then(returnedPerson => {
+          setPersons([...persons, returnedPerson]);
+          setNewName('');
+          setNewNumber('');
+        })
+        .catch(error => {
+          console.error('Error adding person:', error);
+        });
+    }
+  };
+
+  const deletePerson = (id) => {
+    const confirmDelete = window.confirm('Do you really want to delete this person?');
+    if (confirmDelete) {
+      personService.remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id));
+        })
+        .catch(error => {
+          console.error('Error deleting person:', error);
+        });
+    }
+  };
+
+  const updatePerson = (id, newNumber) => {
+    const personToUpdate = persons.find(person => person.id === id);
+
+    if (!personToUpdate) {
+      // Handle the case where the person is not found
+      setErrorMessage(`Person with id ${id} not found`);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+      return;
+    }
+
+    const updatedPerson = { ...personToUpdate, number: newNumber };
+
+    personService
+      .update(id, updatedPerson)
+      .then(returnedPerson => {
+        setPersons(
+          persons.map(person =>
+            person.id !== id ? person : returnedPerson
+          )
+        );
+        setNewName('');
+        setNewNumber('');
+      })
+      .catch(error => {
+        // Handle the error and display a user-friendly message
+        setErrorMessage(`Failed to update the phone number for ${personToUpdate.name}`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+
+  // Function to handle form submission, prevents it from submitting again
+  const storeInfo = (event) => {
+    event.preventDefault();
+    addPerson();
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      {/* Filter component for search functionality */}
+      <Filter
+        searchTerm={searchTerm}
+        onSearchChange={setSearchTerm}
+      />
+
+      <h3>Add a new</h3>
+
+      {/* PersonForm component for adding new people */}
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        onNameChange={setNewName}
+        onNumberChange={setNewNumber}
+        onFormSubmit={storeInfo}
+      />
+
+      <h3>Numbers</h3>
+
+      {/* Persons component for rendering the list of people */}
+      <Persons persons={persons} onDelete={deletePerson} />
+
+      {/* Error message notification */}
+      {errorMessage && (
+        <div className="error">
+          {errorMessage}
+        </div>
+      )}
+    </div>
+  );
 };
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then(response => response.data);
-};
-
-const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then(response => response.data);
-};
-
-const personService = {
-  create,
-  getAll,
-  remove
-};
-
-export default personService;
+export default App;
