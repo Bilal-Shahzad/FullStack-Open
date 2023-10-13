142880b703dfc861a4afce127fb63a2d54fa8782
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-10-12 22:14:21 -0500

    adding all the courses together

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const App = () => {
   const course = {
     id: 1,
@@ -6,22 +8,44 @@ const App = () => {
       {
         name: 'Fundamentals of React',
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
 
-  return <Course course={course} />
-}
+  console.log('Course:', course);
 
-export default App
+  const totalExercises = course.parts.reduce((sum, part) => {
+    console.log('Part:', part);
+    console.log('Current Sum:', sum);
+    return sum + part.exercises;
+  }, 0);
+
+  console.log('Total Exercises:', totalExercises);
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <p>
+        <strong>Total exercises: {totalExercises}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default App;
