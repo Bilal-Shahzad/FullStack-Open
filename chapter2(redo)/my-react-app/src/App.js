19af037268721130d2ecf96464030d1c54dcf85b
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-10-20 23:54:34 -0500

    phone book, prevent default form submission, creating a new person object, update person state, and clear input field

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import Course from './course'; 
+import React, { useState } from 'react';
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault(); 
+    console.log('Form submitted');
+
+    const newPerson = { name: newName };
+    console.log('New person created:', newPerson);
+
+    setPersons([...persons, newPerson]);
+    console.log('Persons state updated:', persons);
+
+    setNewName('');
+    console.log('Input field cleared');
+  }
+
   return (
     <div>
-      <Course />
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default App;
