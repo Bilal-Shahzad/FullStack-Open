a5adbad6f4e2e56c38044c5e5bd5781de70931da
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-10-24 23:27:37 -0500

    adding varaible search term, filterPersons is used to filter poeple based on the term and toLowerCase to make it case insensitive

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,35 +1,47 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  console.log('Rendering App component'); 
-
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
-
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`);
-      console.log(`${newName} is already added to the phonebook`);
     } else {
-      const newPerson = { name: newName, number: newNumber };
-
+      const newPerson = { name: newName, number: newNumber, id: persons.length + 1 };
       setPersons([...persons, newPerson]);
-      console.log(`Added ${newName} to the phonebook`);
 
       setNewName('');
       setNewNumber('');
     }
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  console.log('Persons:', persons);
+  console.log('Search Term:', searchTerm);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <h2>Add a new person</h2>
       <form>
         <div>
           name: <input
@@ -51,8 +63,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
-          <li key={index}>
+        {filteredPersons.map((person) => (
+          <li key={person.id}>
             {person.name} {person.number}
           </li>
         ))}
