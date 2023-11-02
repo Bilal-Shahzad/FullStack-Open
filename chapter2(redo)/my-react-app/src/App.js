b0e3e4975824e3b3798aa58428e232629a4ca0bc
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-11-01 21:57:14 -0500

    having filter, peronform, and persons, seperation makes the code more easier to understand

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,6 +1,57 @@
 import React, { useState } from 'react';
 
+const Filter = ({ searchTerm, handleSearchChange }) => {
+  console.log("Rendering Filter component");
+  return (
+    <div>
+      Search: <input
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+};
+
+const PersonForm = ({ newName, newNumber, handleNameChange, handleNumberChange, addPerson }) => {
+  console.log("Rendering PersonForm component");
+  return (
+    <form>
+      <div>
+        name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit" onClick={addPerson}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ filteredPersons }) => {
+  console.log("Rendering Persons component");
+  return (
+    <ul>
+      {filteredPersons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 const App = () => {
+  console.log("Rendering App component");
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
@@ -29,46 +80,25 @@ const App = () => {
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  console.log('Persons:', persons);
-  console.log('Search Term:', searchTerm);
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Search: <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </div>
-      <h2>Add a new person</h2>
-      <form>
-        <div>
-          name: <input
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>
-            add
-          </button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {filteredPersons.map((person) => (
-          <li key={person.id}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
+
+      <Filter searchTerm={searchTerm} handleSearchChange={(e) => setSearchTerm(e.target.value)} />
+
+      <h3>Add a new person</h3>
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+        addPerson={addPerson}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons filteredPersons={filteredPersons} />
     </div>
   );
 };
