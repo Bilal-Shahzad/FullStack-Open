0fdd0cb5f9abe077285a779083c597537e5a09c8
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-11-15 15:48:36 -0600

    note.js added

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,87 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Note from './components/Note';
+import noteService from './services/notes';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredPersons, setFilteredPersons] = useState([...persons]);
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data);
-        setFilteredPersons(response.data);
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes);
       });
-      // the empty array means the effect runs only once 
-  }, []); 
-  const addPerson = (event) => {
+  }, []);
+
+  const addNote = (event) => {
     event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    };
 
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
-    } else {
-      const newPerson = { name: newName, number: newNumber, id: persons.length + 1 };
-      setPersons([...persons, newPerson]);
-
-      setNewName('');
-      setNewNumber('');
-    }
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote('');
+      });
   };
 
-  const handleSearchChange = (event) => {
-    const searchTerm = event.target.value;
-    setSearchTerm(searchTerm);
-    const updatedFilteredPersons = persons.filter((person) =>
-      person.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredPersons(updatedFilteredPersons);
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => (note.id !== id ? note : returnedNote)));
+      });
   };
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
+      <h1>Notes</h1>
       <div>
-        Search: <input
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
       </div>
-
-      <h3>Add a new</h3>
-
-      <form>
-        <div>
-          name: <input
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </div>
-        <div>
-          number: <input
-            value={newNumber}
-            onChange={(e) => setNewNumber(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>
-            add
-          </button>
-        </div>
-      </form>
-
-      <h3>Numbers</h3>
-
       <ul>
-        {filteredPersons.map((person) => (
-          <li key={person.id}>
-            {person.name} {person.number}
-          </li>
+        {notesToShow.map(note => (
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
