a13ae927d35d14af60ececd15860e2a303bb827b
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-11-22 23:40:40 -0600

    chapter 2 done

--- chapter2(redo)/my-react-app/src/App.js
+++ chapter2(redo)/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 // import Note from './components/Note';
 import noteService from './services/notes';
 import Filter from './components/PhoneBook/Filter';
