1f28ef15e62891df3eb5ce9c5c3ac3c20abf36ae
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-11-29 23:01:57 -0600

    everything up before 3.1

---
+++ chapter 3/phonebook/index.js
@@ -0,0 +1,96 @@
+// imoorts express 
+const express = require('express')
+// creates exxpress application within app
+const app = express()
+
+app.use(express.json())
+// adds middleware to express
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  console.log('GET request to /')
+  res.send('<h1>Hello World!</h1>')
+})
+// sets up route for handline http get requests to root path (/)
+
+app.get('/api/notes', (req, res) => {
+  console.log('GET request to /api/notes')
+  res.json(notes)
+})
+// defines a route for handling HTTP get reuqest to /api/notes
+
+const generateId = () => {
+  // function for generating a new ID 
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  // route for handling HTTP post request to create a new note 
+  console.log('POST request to /api/notes')
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  // route for handling HTTP get to fetch a single note 
+  const id = Number(request.params.id)
+  console.log(`GET request to /api/notes/${id}`)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  // route for handling HTTP DELETE requests a note 
+  const id = Number(request.params.id)
+  console.log(`DELETE request to /api/notes/${id}`)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+// specifies the port the server will be running on
