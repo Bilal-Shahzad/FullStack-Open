4eb699cdb6fc6fe008dd6bfe90f64aa610880068
Bilal-Shahzad <b.shahzad01@gmail.com>
2023-12-04 23:12:03 -0600

    fixing app.get

--- chapter 3/phonebook/index.js
+++ chapter 3/phonebook/index.js
@@ -51,7 +51,7 @@ app.get("/api/persons", (request, response) => {
   response.json(persons)
 })
 
-pp.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
 
