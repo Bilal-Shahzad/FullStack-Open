8205e0ae294de36a6df29b65d1e34136c4e33b36
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-01-10 18:26:06 -0600

    3.13 - fething data from phonebook database

--- chapter 3/phonebook/phonebook-app/mongo.js
+++ chapter 3/phonebook/phonebook-app/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const phonebook = require('./phonebook')
 
 if (process.argv.length<3) {
   console.log('give password as argument')
@@ -8,24 +9,33 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://BILSHAZ:${password}@cluster0.vvqe6kj.mongodb.net/?retryWrites=true&w=majority`
+`mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const phonebookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
+    name: String,
+    number: String,
+  })
 
-const phonebook = mongoose.model('phoneBook', phonebookSchema)
+const phonebook = mongoose.model('phoenBook', phonebookSchema)
 
 const PhoneBook = new phonebook({
-  name: 'Arto Vihavainen',
-  number: '045-1232456',
-})
+    name: 'Arto Vihavainen',
+    number: '045-1232456',
+  })
 
 PhoneBook.save().then(result => {
   console.log('phonebook saved!')
   mongoose.connection.close()
 })
+
+
+phonebook.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+  
