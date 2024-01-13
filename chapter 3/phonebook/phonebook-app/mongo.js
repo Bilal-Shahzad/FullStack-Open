192a007652a7e4ec355a4450e38c99a510bcf113
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-01-13 16:27:51 -0600

    .env file works

--- chapter 3/phonebook/phonebook-app/mongo.js
+++ chapter 3/phonebook/phonebook-app/mongo.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (!process.env.MONGO_PASSWORD) {
+  console.error('MongoDB password not found in environment variables.');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.env.MONGO_PASSWORD;
 
-const url =
-  `mongodb+srv://BILSHAZ:${password}@cluster0.vvqe6kj.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://BILSHAZ:${password}@cluster0.vvqe6kj.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
 
 const phonebookSchema = new mongoose.Schema({
   name: String,
   number: String,
-})
+});
 
-const phonebook = mongoose.model('phoneBook', phonebookSchema)
+const phonebook = mongoose.model('phoneBook', phonebookSchema);
 
 const PhoneBook = new phonebook({
   name: 'Arto Vihavainen',
   number: '045-1232456',
-})
+});
 
 PhoneBook.save().then(result => {
-  console.log('phonebook saved!')
-  mongoose.connection.close()
-})
+  console.log('phonebook saved!');
+  mongoose.connection.close();
+});
